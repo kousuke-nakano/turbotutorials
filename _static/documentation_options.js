898739079d4ce264d76fff61e93d2790b0176877
kousuke-nakano <kousuke-nakano@users.noreply.github.com>
2024-06-09 14:12:44 +0000

    deploy: 25c92ea6b49f0028908bfa53e658ea9f4444fba3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Updated on 03/07/2023',
+    VERSION: 'Updated on 09/06/2024',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
